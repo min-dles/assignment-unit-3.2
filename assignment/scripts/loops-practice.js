04a4e7108a058846b8cd9a6f1cca96e403efd172
mandi andrade <mandi.andrade20@gmail.com>
2023-03-09 17:34:22 -0600

    questions 2a and 3a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -46,13 +46,21 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for(starName of stars){
+  console.log(starName);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let index = 0;
+
+while(stars[index]){
+  console.log(stars[index]);
+  index++;
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');

605618f131b3b0ed15f38d11f864ec15e5822569
mandi andrade <mandi.andrade20@gmail.com>
2023-03-09 16:40:27 -0600

    completed 1a-1d questions

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,18 +13,32 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+// i should still start at 0, need to have function count until i = 6 (then stop).
+for (let i=0; i<6; i++) {  
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+// i needs to start at 3 this time, and loop while i<6 so it ends at 5.
+for (let i=3; i<6; i++) {  
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+// need to start i=2, and instead of increment by 1 with i++, will increment by 2 with i+=2.
+for(let i=2; i<11; i+=2){
+  console.log(i);
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for(let i=5; i>=0; i--){
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
